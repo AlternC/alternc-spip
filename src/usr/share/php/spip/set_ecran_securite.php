fe1e46899491ef1ef646e626f0326987a70c2d86
Camille Lafitte <alternc@webelys.com>
2023-07-20 20:52:16 +0200

    By default disable LOAD check

--- src/usr/share/php/spip/set_ecran_securite.php
+++ src/usr/share/php/spip/set_ecran_securite.php
@@ -1,5 +1,10 @@
 <?php
 
+//By default disable LOAD check feature
+if (!defined('_ECRAN_SECURITE_LOAD')) {
+    define('_ECRAN_SECURITE_LOAD', 0);
+}
+
 if (empty($_SERVER['DISABLE_SPIP_ECRAN_SECU'])) {
     include_once('/usr/share/php/spip/ecran_securite.php');
 }
