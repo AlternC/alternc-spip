e3becfff16f60dd64d06c73398612791fe40e368
Camille Lafitte <alternc@webelys.com>
2023-08-02 12:16:34 +0200

    Check file integrity before to save it
    * As we donwload data from git, we can retrieve its blog sha1
    
    First step check is done with git method

--- src/usr/lib/alternc/update_spip_ecran_securite.sh
+++ src/usr/lib/alternc/update_spip_ecran_securite.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+ecran_securite_file_temp_path=$(mktemp)
 ecran_securite_file_path="/usr/share/php/spip/ecran_securite.php"
 
-wget -qO ${ecran_securite_file_path} https://git.spip.net/spip-contrib-outils/securite/raw/branch/master/ecran_securite.php
-chmod 644 ${ecran_securite_file_path}
+ecran_securite_content_git=$(curl -sX 'GET' \
+  'https://git.spip.net/api/v1/repos/spip-contrib-outils/securite/contents/ecran_securite.php' \
+  -H 'accept: application/json')
+
+ecran_securite_content_sha=$( jq -r  '.sha' <<< "${ecran_securite_content_git}" )
+ecran_securite_content_encoded=$( jq -r  '.content' <<< "${ecran_securite_content_git}" )
+echo "${ecran_securite_content_encoded}" | base64 --decode - > "${ecran_securite_file_temp_path}"
+
+if [[ $(git hash-object "${ecran_securite_file_temp_path}") == "${ecran_securite_content_sha}" ]]; then
+    mv "${ecran_securite_file_temp_path}" "${ecran_securite_file_path}"
+    exit 0
+fi
+
+exit 1
