88be53d8e9d56441f380145c8e1487baf15119f0
Camille Lafitte <alternc@webelys.com>
2023-08-02 12:48:39 +0200

    Check checksum directly from decoded content
    * As content is set in variable we don't need to use a tempory file to do checksum

--- src/usr/lib/alternc/update_spip_ecran_securite.sh
+++ src/usr/lib/alternc/update_spip_ecran_securite.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ecran_securite_file_temp_path=$(mktemp)
 ecran_securite_file_path="/usr/share/php/spip/ecran_securite.php"
 
 ecran_securite_content_git=$(curl -sX 'GET' \
@@ -9,10 +8,10 @@ ecran_securite_content_git=$(curl -sX 'GET' \
 
 ecran_securite_content_sha=$( jq -r  '.sha' <<< "${ecran_securite_content_git}" )
 ecran_securite_content_encoded=$( jq -r  '.content' <<< "${ecran_securite_content_git}" )
-echo "${ecran_securite_content_encoded}" | base64 --decode - > "${ecran_securite_file_temp_path}"
+ecran_securite_content_decoded=$(echo "${ecran_securite_content_encoded}" | base64 --decode - )
 
-if [[ $(git hash-object "${ecran_securite_file_temp_path}") == "${ecran_securite_content_sha}" ]]; then
-    mv "${ecran_securite_file_temp_path}" "${ecran_securite_file_path}"
+if [[ $(echo -n "${ecran_securite_content_decoded}" | git hash-object --stdin) == "${ecran_securite_content_sha}" ]]; then
+    echo -n "${ecran_securite_content_decoded}" > "${ecran_securite_file_path}"
     exit 0
 fi
 
