9af560a0291e4bfcc54ca6eaa9cfd3b586ec7619
Camille Lafitte <alternc@webelys.com>
2024-03-13 17:43:07 +0100

    Script is now hosted on gitlab forge, api has changed
    * Respect new API to retrieve code source

--- src/usr/lib/alternc/update_spip_ecran_securite.sh
+++ src/usr/lib/alternc/update_spip_ecran_securite.sh
@@ -3,18 +3,15 @@
 ecran_securite_file_path="/usr/share/php/spip/ecran_securite.php"
 
 ecran_securite_content_git=$(wget -qO -\
-    --header='accept: application/json' \
-    'https://git.spip.net/api/v1/repos/spip-contrib-outils/securite/contents/ecran_securite.php'\
+    'https://git.spip.net/api/v4/projects/spip-contrib-outils%2Fsecurite/repository/files/ecran_securite.php?ref=master' \
 )
 
-ecran_securite_content_sha=$( jq -r  '.sha' <<< "${ecran_securite_content_git}" )
-ecran_securite_content_size=$( jq -r  '.size' <<< "${ecran_securite_content_git}" )
+ecran_securite_content_sha=$( jq -r  '.content_sha256' <<< "${ecran_securite_content_git}" )
 ecran_securite_content_encoded=$( jq -r  '.content' <<< "${ecran_securite_content_git}" )
 ecran_securite_content_decoded=$(echo "${ecran_securite_content_encoded}" | base64 --decode - )
 
-if [[ $( (echo -ne "blob ${ecran_securite_content_size}\0";echo -n "${ecran_securite_content_decoded}") | sha1sum | head -c 40) == "${ecran_securite_content_sha}" ]]; then
+if [[ $( (echo -n "${ecran_securite_content_decoded}") | sha256sum | head -c 64) == "${ecran_securite_content_sha}" ]]; then
     echo -n "${ecran_securite_content_decoded}" > "${ecran_securite_file_path}"
     exit 0
 fi
-
 exit 1
