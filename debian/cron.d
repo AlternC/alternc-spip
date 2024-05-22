1d721048e299a744c6819a92492ad2f0f7bf6c35
Camille Lafitte <alternc@webelys.com>
2024-05-22 15:01:24 +0200

    fix: [[]] format could break cron execution
    * prefers $(()) format

--- debian/cron.d
+++ debian/cron.d
@@ -1,6 +1,6 @@
 # /etc/cron.d/spip_ecran_secu: crontab entries for the alternc-spip package
 #
-SHELL=/bin/sh
+SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-0 0 * * * root sleep $[ $RANDOM % 86400 ] && /usr/lib/alternc/update_spip_ecran_securite.sh
+0 0 * * * root sleep $(( RANDOM \% 86400 )) && /usr/lib/alternc/update_spip_ecran_securite.sh
